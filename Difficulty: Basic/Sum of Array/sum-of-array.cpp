13f56f40d4dcb32ef308022a841de2ea98f86cbb
Mrunmayee Londhe <131985142+Mrun-mayee3@users.noreply.github.com>
2026-09-21 12:17:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Array/sum-of-array.cpp
+++ Difficulty: Basic/Sum of Array/sum-of-array.cpp
@@ -2,11 +2,17 @@ class Solution {
   public:
     int arraySum(vector<int>& arr) {
         // code here
-        int total = 0;
-        
-        for(int i = 0; i < arr.size(); i++){
-            total+= arr[i];
+        int n = arr.size();
+        int idx = n-1;
+    
+        return fun(arr, idx, n);
+    }
+    
+    int fun(vector<int> & arr, int idx, int n){
+        if(idx == -1){
+            return 0;
         }
-        return total;
+
+        return arr[idx] + fun(arr, idx-1, n);
     }
 };
